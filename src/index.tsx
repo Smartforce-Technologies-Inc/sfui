cd001bb5fae9e4f22669f8fc8c2c8ac0e5456330
pedro.pdc.83 <pedro.pdc.83@gmail.com>
2020-11-19 14:53:30 -0300

    SFIcon and SFIconButton exported

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
+import { format } from 'path';
+
 export * from './Components/SFButton/SFButton';
 export * from './Components/SFCheckbox/SFCheckbox';
+export * from './Components/SFIcon/SFIcon';
+export * from './Components/SFIconButton/SFIconButton';
 export * from './Components/SFPaper/SFPaper';
 export * from './Components/SFRadioGroup/SFRadioGroup';
 export * from './Components/SFSwitch/SFSwitch';
