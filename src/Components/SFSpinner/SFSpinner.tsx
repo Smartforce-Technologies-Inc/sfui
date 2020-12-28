53c0f9c20db04fcd3691bd9837a7bdc228c8844e
Nicolas <nicolas.yopo@smartforcetech.com>
2020-12-28 11:58:07 -0300

    Created SFSpinner

--- src/Components/SFSpinner/SFSpinner.tsx
+++ src/Components/SFSpinner/SFSpinner.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import { withStyles, Theme } from '@material-ui/core/styles';
+import { withStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { CircularProgress, CircularProgressProps } from '@material-ui/core';
 
 const StyledSpinner = withStyles((theme: Theme) => ({
   root: {}
 }))(CircularProgress);
 
+const useSpinnerContainer = makeStyles({
+  root: {
+    display: 'flex',
+    gap: '10px',
+    margin: '5px',
+    alignItems: 'center'
+  }
+});
+
 export interface SFSpinnerProps extends CircularProgressProps {}
 
-export const SFSpinner = ({}: SFSpinnerProps): React.ReactElement<
-  SFSpinnerProps
-> => {
-  return <StyledSpinner></StyledSpinner>;
+export const SFSpinner = ({
+  ...props
+}: SFSpinnerProps): React.ReactElement<SFSpinnerProps> => {
+  const spinnerContainerStyle: Record<'root', string> = useSpinnerContainer();
+  return (
+    <div className={spinnerContainerStyle.root}>
+      <StyledSpinner {...props} variant='indeterminate' />
+    </div>
+  );
 };
