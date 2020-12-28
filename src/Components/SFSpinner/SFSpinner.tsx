9ae9c464c46cdbd67a4ce29be59db36efe9666df
Nicolas <nicolas.yopo@smartforcetech.com>
2020-12-28 15:15:09 -0300

    Fixed color primary

--- src/Components/SFSpinner/SFSpinner.tsx
+++ src/Components/SFSpinner/SFSpinner.tsx
@@ -4,8 +4,9 @@ import { CircularProgress, CircularProgressProps } from '@material-ui/core';
 export interface SFSpinnerProps extends CircularProgressProps {}
 
 export const SFSpinner = ({
-  color = 'primary',
   ...props
 }: SFSpinnerProps): React.ReactElement<SFSpinnerProps> => {
-  return <CircularProgress {...props} variant='indeterminate' />;
+  return (
+    <CircularProgress {...props} color='primary' variant='indeterminate' />
+  );
 };
