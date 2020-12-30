08f8737d198d59c745b4857a8234cec442ee7c67
Martin Weingart <martinweingart@gmail.com>
2020-12-30 09:06:24 -0300

    Extending Dialog components

--- src/Components/SFDialog/SFDialog.tsx
+++ src/Components/SFDialog/SFDialog.tsx
@@ -1,28 +1,64 @@
-import React from 'react';
-import { Dialog, DialogProps } from '@material-ui/core';
-import { withStyles, Theme } from '@material-ui/core/styles';
-
-export {
-  DialogActions as SFDialogActions,
-  DialogActionsProps as SFDialogActionsProps,
-  DialogContent as SFDialogContent,
-  DialogContentProps as SFDialogContentProps,
-  DialogContentText as SFDialogContentText,
-  DialogContentTextProps as SFDialogContentTextProps,
-  DialogTitle as SFDialogTitle,
-  DialogTitleProps as SFDialogTitleProps
+import {
+  Dialog,
+  DialogProps,
+  DialogTitle,
+  DialogTitleProps,
+  DialogContent,
+  DialogContentProps,
+  DialogContentText,
+  DialogContentTextProps,
+  DialogActions,
+  DialogActionsProps
 } from '@material-ui/core';
+import { withStyles, Theme } from '@material-ui/core/styles';
+import { SFGrey } from '../../SFColors/SFColors';
+import { hexToRgba } from '../../helpers';
 
+export interface SFDialogTitleProps extends DialogTitleProps {}
+export interface SFDialogContentProps extends DialogContentProps {}
+export interface SFDialogContentTextProps extends DialogContentTextProps {}
+export interface SFDialogActionsProps extends DialogActionsProps {}
 export interface SFDialogProps extends DialogProps {}
 
-const StyledDialog = withStyles((theme: Theme) => ({
+export const SFDialogTitle = withStyles((theme: Theme) => ({
   root: {
-    background: 'inherit'
+    padding: '24px',
+    '& h2': {
+      color: `${theme.palette.type === 'light' ? SFGrey[900] : SFGrey[50]}`,
+      fontSize: 24,
+      fontStyle: 'normal',
+      fontWeight: 500
+    }
+  }
+}))(DialogTitle);
+
+export const SFDialogContent = withStyles(() => ({
+  root: {
+    padding: '0 24px'
+  }
+}))(DialogContent);
+
+export const SFDialogContentText = withStyles((theme: Theme) => ({
+  root: {
+    color: `${theme.palette.type === 'light' ? SFGrey[900] : SFGrey[50]}`,
+    fontSize: 16,
+    fontStyle: 'normal',
+    fontWeight: 400
+  }
+}))(DialogContentText);
+
+export const SFDialogActions = withStyles(() => ({
+  root: {
+    padding: '24px'
+  }
+}))(DialogActions);
+
+export const SFDialog = withStyles((theme: Theme) => ({
+  root: {
+    backgroundColor: `${
+      theme.palette.type === 'light'
+        ? 'rgba(0, 0, 0, 0.3)'
+        : hexToRgba(SFGrey.A400 as string, 0.8)
+    }`
   }
 }))(Dialog);
-
-export const SFDialog = ({
-  ...props
-}: SFDialogProps): React.ReactElement<SFDialogProps> => {
-  return <StyledDialog {...props} />;
-};
