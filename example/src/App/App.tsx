87df23a6cd6f9f135f3868893c715503a7503a2b
Nicolas <nicolas.yopo@smartforcetech.com>
2021-01-04 16:47:59 -0300

    Added responsiveness to layout

--- example/src/App/App.tsx
+++ example/src/App/App.tsx
@@ -51,88 +51,88 @@ const App = () => {
             backgroundColor: theme.palette.background.default
           }}
         >
-          <h1 className='textHeader'>
-            SmartForce UI Library{' '}
-            <SFSwitch
-              label={switchLabel}
-              checked={nightMode}
-              onChange={toggleSwitch}
-            />
-          </h1>
-          <hr
-            className='contentDivider'
-            style={{ borderColor: theme.palette.primary.main }}
-          />
-          <div className='content firstSection'>
-            <div className='section'>
-              <h4 className='title'>Buttons</h4>
-              <SFButtonsView />
+          <div className='bodyContent'>
+            <h1 className='textHeader'>
+              SmartForce UI Library{' '}
+              <SFSwitch
+                label={switchLabel}
+                checked={nightMode}
+                onChange={toggleSwitch}
+              />
+            </h1>
+            <div className='content firstSection'>
+              <div className='section'>
+                <h4 className='title'>Buttons</h4>
+                <SFButtonsView />
+              </div>
             </div>
-            <div className='section'>
-              <h4 className='title'>Links</h4>
-              <SFLinksView />
+            <div className='content secondSection'>
+              <div className='section'>
+                <h4 className='title'>Icon Button</h4>
+                <SFIconButtonView />
+              </div>
+              <div className='section'>
+                <h4 className='title'>Checkbox</h4>
+                <SFCheckboxesView />
+              </div>
+              <div className='section'>
+                <h4 className='title'>Radio Button</h4>
+                <SFRadioView />
+              </div>
+              <div className='section'>
+                <h4 className='title'>Radio Group Button</h4>
+                <SFRadioGroupView />
+              </div>
+              <div className='section'>
+                <h4 className='title'>Switch</h4>
+                <SFSwitchesView />
+              </div>
             </div>
-            <div className='section'>
-              <h4 className='title'>Progress Circular Indeterminate</h4>
-              <SFSpinnerView />
+            <div className='content thirdSection'>
+              <div className='section'>
+                <h4 className='title'>Links</h4>
+                <SFLinksView />
+              </div>
+              <div className='section'>
+                <h4 className='title'>Progress Circular Indeterminate</h4>
+                <SFSpinnerView />
+              </div>
             </div>
-          </div>
-          <div className='content secondSection'>
-            <div className='section'>
-              <h4 className='title'>Icon Button</h4>
-              <SFIconButtonView />
+            <div className='content fourthSection'>
+              <div className='section'>
+                <h4 className='title'>Text Field</h4>
+                <SFTextFieldsView />
+              </div>
+              <div className='section'>
+                <h4 className='title'>Date Picker</h4>
+                <SFDatePickerView />
+              </div>
             </div>
-            <div className='section'>
-              <h4 className='title'>Checkbox</h4>
-              <SFCheckboxesView />
+            <div className='content fifthSection'>
+              <div className='section'>
+                <h4 className='title'>Text Field Multiline</h4>
+                <SFTextFieldsMultilineView />
+              </div>
+              <div className='section'>
+                <h4 className='title'>Dialog</h4>
+                <SFDialogsView />
+              </div>
             </div>
-            <div className='section'>
-              <h4 className='title'>Radio Button</h4>
-              <SFRadioView />
+            <div className='content sixthSection'>
+              <div className='section'>
+                <h4 className='title'>Simple Select</h4>
+                <SFSelectView />
+              </div>
+              <div className='section'>
+                <h4 className='title'>Multiline Select</h4>
+                <SFMultiSelectView />
+              </div>
             </div>
-            <div className='section'>
-              <h4 className='title'>Radio Group Button</h4>
-              <SFRadioGroupView />
-            </div>
-            <div className='section'>
-              <h4 className='title'>Switch</h4>
-              <SFSwitchesView />
-            </div>
-          </div>
-          <div className='content thirdSection'>
-            <div className='section'>
-              <h4 className='title'>Text Field</h4>
-              <SFTextFieldsView />
-            </div>
-            <div className='section'>
-              <h4 className='title'>Date Picker</h4>
-              <SFDatePickerView />
-            </div>
-          </div>
-          <div className='content fourthSection'>
-            <div className='section'>
-              <h4 className='title'>Text Field Multiline</h4>
-              <SFTextFieldsMultilineView />
-            </div>
-            <div className='section'>
-              <h4 className='title'>Dialog</h4>
-              <SFDialogsView />
-            </div>
-          </div>
-          <div className='content thirdSection'>
-            <div className='section'>
-              <h4 className='title'>Simple Select</h4>
-              <SFSelectView />
-            </div>
-            <div className='section'>
-              <h4 className='title'>Multiline Select</h4>
-              <SFMultiSelectView />
-            </div>
-          </div>
-          <div className='content fifthSection'>
-            <div className='section'>
-              <h4>Tables</h4>
-              <SFTableView />
+            <div className='content seventhSection'>
+              <div className='section'>
+                <h4>Tables</h4>
+                <SFTableView />
+              </div>
             </div>
           </div>
         </SFPaper>
