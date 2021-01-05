7090e3fe9a21b6888508c250278e936e6358dc59
Martin Weingart <martinweingart@gmail.com>
2021-01-05 08:41:57 -0300

    Remove SFCheckbox unnecesary stories and controls

--- src/Components/SFCheckbox/SFCheckbox.stories.tsx
+++ src/Components/SFCheckbox/SFCheckbox.stories.tsx
@@ -6,25 +6,57 @@ import { SFCheckbox, SFCheckboxProps } from './SFCheckbox';
 
 export default {
   title: 'Components/SFCheckbox',
-  component: SFCheckbox
+  component: SFCheckbox,
+  args: {
+    label: 'Bagel'
+  },
+  argTypes: {
+    onChange: {
+      action: 'onChange',
+      table: {
+        disable: true
+      }
+    },
+    checked: {
+      defaultValue: true,
+      control: {
+        type: 'boolean'
+      }
+    },
+    disabled: {
+      defaultValue: false,
+      control: {
+        type: 'boolean'
+      }
+    },
+    ref: {
+      table: {
+        disable: true
+      }
+    },
+    action: {
+      table: {
+        disable: true
+      }
+    },
+    focusVisibleClassName: {
+      table: {
+        disable: true
+      }
+    },
+    onFocusVisible: {
+      table: {
+        disable: true
+      }
+    },
+    TouchRippleProps: {
+      table: {
+        disable: true
+      }
+    }
+  }
 } as Meta;
 
 const Template: Story<SFCheckboxProps> = (args) => <SFCheckbox {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {
-  label: 'Bagel'
-};
-
-export const Desabled = Template.bind({});
-Desabled.args = {
-  label: 'Bagel',
-  disabled: true
-};
-
-export const DesabledChecked = Template.bind({});
-DesabledChecked.args = {
-  label: 'Bagel',
-  disabled: true,
-  checked: true
-};
