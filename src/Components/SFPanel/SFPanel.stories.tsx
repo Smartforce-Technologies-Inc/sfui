984046599f8b01c40c861a0886aca6963440f534
Martin Weingart <martinweingart@gmail.com>
2021-01-06 13:44:45 -0300

    Added SFPanel component and stories. Remove SFDrawer stories.

---
+++ src/Components/SFPanel/SFPanel.stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { SFPanel, SFPanelProps } from './SFPanel';
+import { SFTextField } from '../SFTextField/SFTextField';
+
+export default {
+  title: 'Components/SFPanel',
+  component: SFPanel,
+  args: {
+    title: 'Drawer title',
+    open: true
+  },
+  argTypes: {
+    onClose: {
+      action: 'onClose',
+      table: {
+        disable: true
+      }
+    },
+    anchor: {
+      defaultValue: 'right',
+      control: {
+        type: 'radio',
+        options: ['left', 'right', 'bottom', 'top']
+      }
+    },
+    elevation: {
+      defaultValue: 16,
+      control: {
+        type: 'number'
+      }
+    }
+  }
+} as Meta;
+
+export const Panel = (args: SFPanelProps): JSX.Element => (
+  <SFPanel
+    {...args}
+    leftAction={{ label: 'Medium' }}
+    rightAction={{ label: 'Medium' }}
+  >
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
+    Mauris lobortis a erat eu mattis.
+    {/* <SFTextField style={{ marginTop: 24 }} label='Bagel' />
+    <SFTextField style={{ marginTop: 12 }} label='Bagel' />
+    <SFTextField style={{ marginTop: 12 }} label='Bagel' />
+    <SFTextField style={{ marginTop: 12 }} label='Bagel' /> */}
+  </SFPanel>
+);
