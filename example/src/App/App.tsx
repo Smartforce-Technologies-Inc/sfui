7bc42247c72cf23dbeb7870d4db766813c14343c
Nicolas <nicolas.yopo@smartforcetech.com>
2021-01-11 16:34:38 -0300

    Created Views in example/Added Styles & Components
    Views created:
    
    - Created a pages folder containing componentsPage and demoPage
    - ComponentsPage contains all components of example
    - Added demo Form to demoPage
    
    Refactored App Wrapper:
    
    - Added pages state to switch between component and demo page
    
    Styles Added:
    
    - Added demo page styling
    - Added form demo styling

--- example/src/App/App.tsx
+++ example/src/App/App.tsx
@@ -7,24 +7,12 @@ import {
   SFPaper,
   SFSwitch,
   useSFMediaQuery,
-  SFStylesProvider
+  SFStylesProvider,
+  SFLink
 } from 'sfui';
 
-import { SFButtonsView } from './ButtonsView/ButtonsView';
-import { SFCheckboxesView } from './CheckboxesView/CheckboxesView';
-import { SFDatePickerView } from './DatePickerView/DatePickerView';
-import { SFDialogsView } from './DialogsView/DialogsView';
-import { SFIconButtonView } from './IconButtonView/IconButtonView';
-import { SFLinksView } from './LinksView/LinksView';
-import { SFMultiSelectView } from './MultiSelectView/MultiSelectView';
-import { SFRadioView } from './RadioView/RadioView';
-import { SFRadioGroupView } from './RadioGroupView/RadioGroupView';
-import { SFSpinnerView } from './SpinnerView/SpinnerView';
-import { SFSelectView } from './SelectView/SelectView';
-import { SFSwitchesView } from './SwitchesView/SwitchesView';
-import { SFTextFieldsView } from './TextFieldsView/TextFieldsView';
-import { SFTextFieldsMultilineView } from './TextFieldsMultilineView/TextFieldsMultilineView';
-import { SFTableView } from './TableView/TableView';
+import { ComponentsPage } from './Pages/ComponentsPage';
+import { DemosPage } from './Pages/DemosPage';
 
 const App = (): JSX.Element => {
   const prefersDarkMode: boolean = useSFMediaQuery(
@@ -32,6 +20,7 @@ const App = (): JSX.Element => {
   );
 
   const [nightMode, setNightMode] = useState(prefersDarkMode);
+  const [showDemo, setShowDemo] = useState(false);
 
   const switchLabel = nightMode === true ? 'Day Mode' : 'Night Mode';
 
@@ -58,80 +47,15 @@ const App = (): JSX.Element => {
                 onChange={toggleSwitch}
               />
             </h1>
-            <div className='content firstSection'>
-              <div className='section'>
-                <h4 className='title'>Buttons</h4>
-                <SFButtonsView />
-              </div>
+            <div className='pageSelector'>
+              <SFLink sfSize='medium' onClick={() => setShowDemo(false)}>
+                Components
+              </SFLink>
+              <SFLink sfSize='medium' onClick={() => setShowDemo(true)}>
+                Demos
+              </SFLink>
             </div>
-            <div className='content secondSection'>
-              <div className='section'>
-                <h4 className='title'>Icon Button</h4>
-                <SFIconButtonView />
-              </div>
-              <div className='section'>
-                <h4 className='title'>Checkbox</h4>
-                <SFCheckboxesView />
-              </div>
-              <div className='section'>
-                <h4 className='title'>Radio Button</h4>
-                <SFRadioView />
-              </div>
-              <div className='section'>
-                <h4 className='title'>Radio Group Button</h4>
-                <SFRadioGroupView />
-              </div>
-              <div className='section'>
-                <h4 className='title'>Switch</h4>
-                <SFSwitchesView />
-              </div>
-            </div>
-            <div className='content thirdSection'>
-              <div className='section'>
-                <h4 className='title'>Links</h4>
-                <SFLinksView />
-              </div>
-              <div className='section'>
-                <h4 className='title'>Progress Circular Indeterminate</h4>
-                <SFSpinnerView />
-              </div>
-            </div>
-            <div className='content fourthSection'>
-              <div className='section'>
-                <h4 className='title'>Text Field</h4>
-                <SFTextFieldsView />
-              </div>
-              <div className='section'>
-                <h4 className='title'>Date Picker</h4>
-                <SFDatePickerView />
-              </div>
-            </div>
-            <div className='content fifthSection'>
-              <div className='section'>
-                <h4 className='title'>Text Field Multiline</h4>
-                <SFTextFieldsMultilineView />
-              </div>
-              <div className='section'>
-                <h4 className='title'>Dialog</h4>
-                <SFDialogsView />
-              </div>
-            </div>
-            <div className='content sixthSection'>
-              <div className='section'>
-                <h4 className='title'>Simple Select</h4>
-                <SFSelectView />
-              </div>
-              <div className='section'>
-                <h4 className='title'>Multiline Select</h4>
-                <SFMultiSelectView />
-              </div>
-            </div>
-            <div className='content seventhSection'>
-              <div className='section'>
-                <h4>Tables</h4>
-                <SFTableView />
-              </div>
-            </div>
+            {showDemo === false ? <ComponentsPage /> : <DemosPage />}
           </div>
         </SFPaper>
       </SFStylesProvider>
