09d6ef008616c6f00da984686311f33b4682157e
Martin Weingart <martinweingart@gmail.com>
2021-02-11 12:16:51 -0300

    Add SFMenuItem component

---
+++ src/Components/SFMenuItem/SFMenuItem.tsx
@@ -0,0 +1,35 @@
+import { withStyles, Theme } from '@material-ui/core/styles';
+import { MenuItem } from '@material-ui/core';
+import { hexToRgba } from '../../helpers';
+import { SFGrey } from '../../SFColors/SFColors';
+
+export const SFMenuItem = withStyles((theme: Theme) => ({
+  root: {
+    padding: '6px 24px',
+
+    '&:hover': {
+      background:
+        theme.palette.type === 'light'
+          ? hexToRgba(SFGrey[200], 0.3)
+          : hexToRgba(SFGrey[500], 0.3)
+    },
+    '&:active': {
+      background:
+        theme.palette.type === 'light'
+          ? hexToRgba(SFGrey[200], 0.5)
+          : hexToRgba(SFGrey[500], 0.2)
+    },
+    '&.Mui-selected': {
+      background:
+        theme.palette.type === 'light'
+          ? hexToRgba(SFGrey[200], 0.5)
+          : hexToRgba(SFGrey[500], 0.2),
+      '&:hover': {
+        background:
+          theme.palette.type === 'light'
+            ? hexToRgba(SFGrey[200], 0.3)
+            : hexToRgba(SFGrey[500], 0.3)
+      }
+    }
+  }
+}))(MenuItem);
