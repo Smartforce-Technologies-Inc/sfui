950af2b3f8e2d0b20bee9676684acf8e58449b45
pedro.pdc.83 <pedro.pdc.83@gmail.com>
2021-04-13 09:16:22 -0300

    Rotate icons functionality
    - rotate prop added for SFIcon and SFIconButton

--- src/Components/SFIcon/SFIcon.tsx
+++ src/Components/SFIcon/SFIcon.tsx
@@ -4,23 +4,43 @@ import { SFGrey } from '../../SFColors/SFColors';
 import IcomoonReact from 'icomoon-react';
 import IconSet from './icons/selection.json';
 
+export type SFIconRotation = 'left' | 'right' | 'invert' | 'none';
+
 export interface SFIconProps {
   color?: string | undefined;
   size?: string | number | undefined;
+  rotate?: SFIconRotation;
   icon: string;
   className?: string | undefined;
   style?: React.CSSProperties | undefined;
 }
 
+const getRotationDeg = (rotation: SFIconRotation): number => {
+  switch (rotation) {
+    case 'left':
+      return -90;
+    case 'right':
+      return 90;
+    case 'invert':
+      return 180;
+    default:
+      return 0;
+      break;
+  }
+};
+
 export const SFIcon = forwardRef(
   (
-    { icon = 'Bell', size = 24, ...props }: SFIconProps,
+    { icon = 'Bell', size = 24, rotate = 'none', ...props }: SFIconProps,
     ref: React.Ref<SVGSVGElement>
   ): React.ReactElement<SFIconProps> => {
     const theme: Theme = useTheme();
     const colorDefault: string =
       theme.palette.type === 'light' ? SFGrey[600] : SFGrey[400];
     const color: string = props.color ? props.color : colorDefault;
+    const customStyle: React.CSSProperties = {
+      transform: `rotate(${getRotationDeg(rotate)}deg)`
+    };
 
     return (
       <IcomoonReact
@@ -29,6 +49,7 @@ export const SFIcon = forwardRef(
         icon={icon}
         size={size}
         color={color}
+        style={customStyle}
         ref={ref}
       />
     );
--- src/Components/SFIconButton/SFIconButton.tsx
+++ src/Components/SFIconButton/SFIconButton.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useTheme, Theme, withStyles } from '@material-ui/core/styles';
 import { SFGrey } from '../../SFColors/SFColors';
-import { SFIcon } from '../SFIcon/SFIcon';
+import { SFIcon, SFIconRotation } from '../SFIcon/SFIcon';
 import IconButton, { IconButtonProps } from '@material-ui/core/IconButton';
 
 type SFSize = 'tiny' | 'small' | 'medium' | 'large';
@@ -59,12 +59,14 @@ export interface SFIconButtonProps extends IconButtonProps {
   sfColor?: string | undefined;
   sfSize: SFSize;
   sfIcon: string;
+  rotate?: SFIconRotation;
 }
 
 export const SFIconButton = ({
   sfColor,
   sfSize = 'medium',
   sfIcon = 'Bell',
+  rotate = 'none',
   ...props
 }: SFIconButtonProps): React.ReactElement<SFIconButtonProps> => {
   const theme: Theme = useTheme();
@@ -86,6 +88,7 @@ export const SFIconButton = ({
         icon={sfIcon}
         size={iconButtonInnerProps.size}
         color={props.disabled ? disabledColor : colorPicked}
+        rotate={rotate}
       />
     </StyledIconButton>
   );
