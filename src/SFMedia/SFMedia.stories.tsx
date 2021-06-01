af6aa9267552b4044a18f93927f66d5f5cbba708
Martin Weingart <martinweingart@gmail.com>
2021-06-01 15:58:34 -0300

    SFMedia added story for Sass variables

--- src/SFMedia/SFMedia.stories.tsx
+++ src/SFMedia/SFMedia.stories.tsx
@@ -14,12 +14,21 @@ const useStyles = makeStyles({
   value: { textAlign: 'right' }
 });
 
-export const Breakpoints: Story = () => {
+export const BreakpointsEnum: Story = () => {
   const classes = useStyles();
 
   return (
     <div>
       <h2>Media Queries Breakpoints</h2>
+
+      <p>
+        List of SFMedia Enum values for media queries breakpoint. Can be
+        imported from the library.
+      </p>
+
+      <code>import SFMedia from 'sfui';</code>
+      <br />
+      <code>const extra_small_width = SFMedia.XS_WIDTH</code>
 
       <div className={classes.container}>
         <div>
@@ -78,6 +87,88 @@ export const Breakpoints: Story = () => {
             <span>XXL_WIDTH</span>
             <span className={classes.value}>{SFMedia.XXL_WIDTH}</span>
             <span>XXL_HEIGHT</span>
+            <span className={classes.value}>{SFMedia.XXL_HEIGHT}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const SassVariables: Story = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h2>Media Queries Breakpoints</h2>
+
+      <p>
+        List of Sass Variables values for media queries breakpoint. Can be
+        imported from the library into a sass module.
+      </p>
+
+      <code>@import '~sfui/dist/styles/SFMedia/SFMedia.module.scss';</code>
+      <br />
+      <br />
+      <code>{'@media screen and (min-width: $sf-media-xs-width) {  }'}</code>
+
+      <div className={classes.container}>
+        <div>
+          <h3>Extra Small</h3>
+          <div className={classes.media}>
+            <span>$sf-media-xs-width</span>
+            <span className={classes.value}>{SFMedia.XS_WIDTH}</span>
+            <span>$sf-media-xs-height</span>
+            <span className={classes.value}>{SFMedia.XS_HEIGHT}</span>
+          </div>
+        </div>
+
+        <div>
+          <h3>Small</h3>
+          <div className={classes.media}>
+            <span>$sf-media-sm-width</span>
+            <span className={classes.value}>{SFMedia.SM_WIDTH}</span>
+            <span>$sf-media-sm-height</span>
+            <span className={classes.value}>{SFMedia.SM_HEIGHT}</span>
+          </div>
+        </div>
+
+        <div>
+          <h3>Medium</h3>
+          <div className={classes.media}>
+            <span>$sf-media-md-width</span>
+            <span className={classes.value}>{SFMedia.MD_WIDTH}</span>
+            <span>$sf-media-md-height</span>
+            <span className={classes.value}>{SFMedia.MD_HEIGHT}</span>
+          </div>
+        </div>
+
+        <div>
+          <h3>Large</h3>
+          <div className={classes.media}>
+            <span>$sf-media-lg-width</span>
+            <span className={classes.value}>{SFMedia.LG_WIDTH}</span>
+            <span>$sf-media-lg-height</span>
+            <span className={classes.value}>{SFMedia.LG_HEIGHT}</span>
+          </div>
+        </div>
+
+        <div>
+          <h3>Extra Large</h3>
+          <div className={classes.media}>
+            <span>$sf-media-xl-width</span>
+            <span className={classes.value}>{SFMedia.XL_WIDTH}</span>
+            <span>$sf-media-xl-height</span>
+            <span className={classes.value}>{SFMedia.XL_HEIGHT}</span>
+          </div>
+        </div>
+
+        <div>
+          <h3>Extra Extra Large</h3>
+          <div className={classes.media}>
+            <span>$sf-media-xxl-width</span>
+            <span className={classes.value}>{SFMedia.XXL_WIDTH}</span>
+            <span>$sf-media-xxl-height</span>
             <span className={classes.value}>{SFMedia.XXL_HEIGHT}</span>
           </div>
         </div>
