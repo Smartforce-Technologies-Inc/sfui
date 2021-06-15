55d61622aa97c9db8503bdc407815ddd9b6f5885
Yopo Nicolas <nicolas.yopo@smartforcetech.com>
2021-06-15 14:52:13 -0300

    Refactored chip modal, added useEffect hook to handle value change

--- src/Components/SFChipsListInput/SFChipListModal/SFChipListModal.tsx
+++ src/Components/SFChipsListInput/SFChipListModal/SFChipListModal.tsx
@@ -1,82 +1,77 @@
 import React from 'react';
 import { SFAlert } from '../../SFAlert/SFAlert';
 import { SFTextField } from '../../SFTextField/SFTextField';
-import { ValueType } from '../SFChipsListInput';
+import { ChipListValueType } from '../SFChipsListInput';
 
-export type EditChipValue = {
-  type: string;
-  input: ValueType;
+export type EditChipListValue = {
+  label: string;
+  input: ChipListValueType;
   index: number;
-};
-
-export const emptyEditValue = {
-  type: '',
-  input: {
-    value: ''
-  },
-  index: -1
 };
 
 export interface SFChipListModalProps {
-  editValue: ValueType;
-  index: number;
-  type: string;
+  editValue: EditChipListValue;
   open: boolean;
-  onEdit: (value: ValueType, index: number) => void;
+  onEdit: (value: ChipListValueType, index: number) => void;
   onClose: () => void;
 }
 
 export const SFChipListModal = ({
   editValue,
-  index,
-  type,
   open,
   onEdit,
   onClose
 }: SFChipListModalProps): React.ReactElement<SFChipListModalProps> => {
   const [isEditing, setIsEditing] = React.useState<boolean>(false);
-  const [value, setValue] = React.useState<ValueType>(editValue);
+  const [editedValue, setEditedValue] = React.useState<ChipListValueType>(
+    editValue.input
+  );
 
   React.useEffect(() => {
-    setValue(editValue);
+    setEditedValue(editValue.input);
   }, [editValue]);
+
+  const onInputChange = (input: React.ChangeEvent<HTMLInputElement>): void => {
+    setIsEditing(true);
+    setEditedValue({
+      value: input.target.value,
+      isNew: editValue.input.isNew,
+      hasChanged: true
+    });
+  };
+
+  const onFinishEdition = (): void => {
+    onEdit(editedValue, editValue.index);
+    onClose();
+  };
 
   return (
     <SFAlert
-      title={`Edit ${type}`}
+      title={`Edit ${editValue && editValue.label}`}
       rightAction={{
         label: 'Done',
         buttonProps: {
           disabled: !isEditing,
-          onClick: (): void => {
-            onEdit(value, index);
-            onClose();
-          }
+          onClick: (): void => onFinishEdition()
         }
       }}
       leftAction={{
         label: 'Cancel',
         buttonProps: {
-          onClick: (): void => {
-            onClose();
-            setValue(emptyEditValue.input);
-          }
+          onClick: (): void => onClose()
         }
       }}
       open={open}
-      content={`To edit this ${type}, please enter the new one here.`}
+      content={`To edit this ${
+        editValue && editValue.label
+      }, please enter the new one here.`}
     >
       <SFTextField
-        label={type}
-        value={value.value}
-        onChange={(input: React.ChangeEvent<HTMLInputElement>): void => {
-          setIsEditing(true);
-          setValue({
-            value: input.target.value,
-            isNew: editValue.isNew,
-            hasChanged: true
-          });
-        }}
+        label={editValue && editValue.label}
+        value={editedValue.value}
+        onChange={(input: React.ChangeEvent<HTMLInputElement>): void =>
+          onInputChange(input)
+        }
       />
     </SFAlert>
   );
