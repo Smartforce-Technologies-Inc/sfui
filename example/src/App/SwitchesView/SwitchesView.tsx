4820f4f2887916370e3ec3e380c0495911378a12
Yopo Nicolas <nicolas.yopo@smartforcetech.com>
2021-07-12 15:15:08 -0300

    Fixed visual layout issues in example on different resolutions

--- example/src/App/SwitchesView/SwitchesView.tsx
+++ example/src/App/SwitchesView/SwitchesView.tsx
@@ -4,30 +4,34 @@ import { SFSwitch } from 'sfui';
 
 const SFSwitchesView = (): JSX.Element => {
   return (
-    <div className='row'>
-      <div className='column'>
-        <SFSwitch size='small' />
-        <SFSwitch size='small' disabled />
-        <SFSwitch size='small' checked />
-        <SFSwitch size='small' checked disabled />
+    <div className='row extra-space'>
+      <div className='row'>
+        <div className='column'>
+          <SFSwitch size='small' />
+          <SFSwitch size='small' disabled />
+          <SFSwitch size='small' checked />
+          <SFSwitch size='small' checked disabled />
+        </div>
+        <div className='column'>
+          <SFSwitch size='small' label='Bagel' />
+          <SFSwitch size='small' label='Bagel' disabled />
+          <SFSwitch size='small' label='Bagel' checked />
+          <SFSwitch size='small' checked label='Bagel' disabled />
+        </div>
       </div>
-      <div className='column'>
-        <SFSwitch size='small' label='Bagel' />
-        <SFSwitch size='small' label='Bagel' disabled />
-        <SFSwitch size='small' label='Bagel' checked />
-        <SFSwitch size='small' checked label='Bagel' disabled />
-      </div>
-      <div className='column'>
-        <SFSwitch />
-        <SFSwitch disabled />
-        <SFSwitch checked />
-        <SFSwitch checked disabled />
-      </div>
-      <div className='column'>
-        <SFSwitch label='Bagel' />
-        <SFSwitch label='Bagel' disabled />
-        <SFSwitch label='Bagel' checked />
-        <SFSwitch checked label='Bagel' disabled />
+      <div className='row'>
+        <div className='column'>
+          <SFSwitch />
+          <SFSwitch disabled />
+          <SFSwitch checked />
+          <SFSwitch checked disabled />
+        </div>
+        <div className='column'>
+          <SFSwitch label='Bagel' />
+          <SFSwitch label='Bagel' disabled />
+          <SFSwitch label='Bagel' checked />
+          <SFSwitch checked label='Bagel' disabled />
+        </div>
       </div>
     </div>
   );
