669dd20a1d2b465b6b0674d4f4c2dae0816b0788
Yopo Nicolas <nicolas.yopo@smartforcetech.com>
2021-08-24 12:54:13 -0300

    Created SFAlert component and styles

--- src/Components/SFAlert/SFAlert.tsx
+++ src/Components/SFAlert/SFAlert.tsx
@@ -1,59 +1,83 @@
 import React from 'react';
-import {
-  SFDialog,
-  SFDialogProps,
-  SFDialogActions,
-  SFDialogContent,
-  SFDialogContentText,
-  SFDialogTitle
-} from '../SFDialog/SFDialog';
-import { SFButton, SFButtonProps } from '../SFButton/SFButton';
+import { Alert, AlertTitle, AlertProps } from '@material-ui/lab';
+import { SFIcon } from '../SFIcon/SFIcon';
+import { SFIconButton } from '../SFIconButton/SFIconButton';
+import { Theme, withStyles, makeStyles } from '@material-ui/core/styles';
+import { SFRed } from '../../SFColors/SFColors';
+import { hexToRgba } from '../../Helpers';
 
-export interface SFAlertProps extends SFDialogProps {
+const StyledIconButton = withStyles((theme: Theme) => ({
+  root: {
+    '& .MuiIconButton-label': {
+      '& svg': {
+        fill: theme.palette.type === 'light' ? SFRed[900] : SFRed[50]
+      }
+    },
+
+    '&:hover': {
+      backgroundColor:
+        theme.palette.type === 'light'
+          ? hexToRgba(SFRed[100], 0.4)
+          : hexToRgba(SFRed[200], 0.2)
+    },
+
+    '&:active': {
+      backgroundColor:
+        theme.palette.type === 'light'
+          ? hexToRgba(SFRed[100], 0.7)
+          : hexToRgba(SFRed[200], 0.1)
+    }
+  }
+}))(SFIconButton);
+
+const StyledAlert = withStyles((theme: Theme) => ({
+  root: {
+    padding: '16px',
+    backgroundColor: theme.palette.type === 'light' ? SFRed[50] : SFRed[900],
+
+    '& .MuiAlert-message': {
+      color: theme.palette.type === 'light' ? SFRed[900] : SFRed[50],
+      padding: '0px 24px 0px 16px',
+      fontWeight: 400,
+      fontSize: '16px',
+      lineHeight: '24px',
+
+      '& .MuiAlertTitle-root': {
+        fontWeight: 700
+      }
+    },
+
+    '& .MuiAlert-icon': {
+      padding: '0px',
+      margin: '0px',
+
+      '& svg': {
+        fill: theme.palette.type === 'light' ? SFRed[700] : SFRed[200]
+      }
+    }
+  }
+}))(Alert);
+
+export interface SFAlertProps extends AlertProps {
   title: string;
-  content: string;
-  leftAction?: SFAlertAction;
-  rightAction?: SFAlertAction;
-}
-
-export interface SFAlertAction {
-  label: string;
-  buttonProps?: SFButtonProps;
+  text?: string;
+  onClose?: () => void;
 }
 
 export const SFAlert = ({
   title,
-  content,
-  leftAction,
-  rightAction,
-  children,
-  ...props
+  text,
+  onClose
 }: SFAlertProps): React.ReactElement<SFAlertProps> => {
   return (
-    <div>
-      <SFDialog {...props}>
-        <SFDialogTitle>{title}</SFDialogTitle>
-
-        <SFDialogContent>
-          <SFDialogContentText>{content}</SFDialogContentText>
-
-          {children}
-        </SFDialogContent>
-
-        <SFDialogActions>
-          {leftAction && (
-            <SFButton sfColor='grey' variant='text' {...leftAction.buttonProps}>
-              {leftAction.label}
-            </SFButton>
-          )}
-
-          {rightAction && (
-            <SFButton sfColor='blue' {...rightAction.buttonProps}>
-              {rightAction.label}
-            </SFButton>
-          )}
-        </SFDialogActions>
-      </SFDialog>
-    </div>
+    <StyledAlert
+      icon={<SFIcon icon='Error-Mark' />}
+      severity='error'
+      action={<StyledIconButton sfIcon='Close' sfSize='medium' />}
+      onClose={onClose}
+    >
+      <AlertTitle>{title}</AlertTitle>
+      {text}
+    </StyledAlert>
   );
 };
