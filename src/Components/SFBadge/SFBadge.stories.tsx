3b9d696dac8d73626c1b0fde8ff4f0cf69e5c3ac
Pedro Castano <pedro.pdc.83@gmail.com>
2021-11-11 16:46:57 -0300

    SFBadge story usage explanation added

--- src/Components/SFBadge/SFBadge.stories.tsx
+++ src/Components/SFBadge/SFBadge.stories.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { SFBadge } from './SFBadge';
 import { SFIcon, SFIconButton, SFLink } from '../..';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 
 const AnchorOrigins = {
   topLeft: { vertical: 'top', horizontal: 'left' },
@@ -9,6 +10,72 @@ const AnchorOrigins = {
   bottomLeft: { vertical: 'bottom', horizontal: 'left' },
   bottomRight: { vertical: 'bottom', horizontal: 'right' }
 };
+
+const headerNote = (): JSX.Element => {
+  return (
+    <div>
+      <p>
+        <span>
+          <b>NOTE: </b>
+        </span>
+        You should adjust the badge absolute position, depending on the
+        anchorOrigin value set, inside your application code.
+      </p>
+      <p>For example, if you set: </p>
+      <code>anchorOrigin={`{ vertical: 'top', horizontal: 'left' }`}</code>
+      <p>you should be to adjust the following class:</p>
+      <code>
+        .MuiBadge-anchorOriginTopLeftCircle{' '}
+        {`{
+          top: '14%';
+          left: '14%';
+          }`}
+      </code>
+    </div>
+  );
+};
+
+const useCircleStyles = makeStyles((theme: Theme) => ({
+  custom: {
+    '& .MuiBadge-anchorOriginTopLeftCircle': {
+      top: '28%',
+      left: '28%'
+    },
+    '& .MuiBadge-anchorOriginTopRightCircle': {
+      top: '28%',
+      right: '28%'
+    },
+    '& .MuiBadge-anchorOriginBottomLeftCircle': {
+      bottom: '28%',
+      left: '28%'
+    },
+    '& .MuiBadge-anchorOriginBottomRightCircle': {
+      bottom: '28%',
+      right: '28%'
+    }
+  }
+}));
+
+const useRectangleStyles = makeStyles((theme: Theme) => ({
+  custom: {
+    '& .MuiBadge-anchorOriginTopLeftRectangle': {
+      top: '-7%',
+      left: '-7%'
+    },
+    '& .MuiBadge-anchorOriginTopRightRectangle': {
+      top: '-7%',
+      right: '-7%'
+    },
+    '& .MuiBadge-anchorOriginBottomLeftRectangle': {
+      bottom: '-7%',
+      left: '-7%'
+    },
+    '& .MuiBadge-anchorOriginBottomRightRectangle': {
+      bottom: '-7%',
+      right: '-7%'
+    }
+  }
+}));
 
 export default {
   title: 'Components/SFBadge',
@@ -84,27 +151,61 @@ export default {
   }
 } as Meta;
 
-export const BadgeCircular = (args): JSX.Element => (
-  <div style={{ display: 'flex', gap: '30px', alignItems: 'center' }}>
-    <SFBadge {...args}>
-      <SFIcon icon='Bell' />
-    </SFBadge>
-    <SFBadge {...args}>
-      <SFIconButton sfSize='medium' sfIcon='Bell' />
-    </SFBadge>
-  </div>
-);
+export const BadgeCircular = (args): JSX.Element => {
+  const style = useCircleStyles();
 
-export const BadgeRectangular = (args): JSX.Element => (
-  <div style={{ display: 'flex', gap: '30px', alignItems: 'center' }}>
-    <SFBadge {...args}>
-      <p style={{ margin: 0, padding: 0 }}>Text</p>
-    </SFBadge>
-    <SFBadge {...args}>
-      <SFLink sfSize='medium'>Some Link</SFLink>
-    </SFBadge>
-  </div>
-);
+  return (
+    <Fragment>
+      {headerNote()}
+      <h4>Default config</h4>
+      <div style={{ display: 'flex', gap: '30px', alignItems: 'center' }}>
+        <SFBadge {...args}>
+          <SFIcon icon='Bell' />
+        </SFBadge>
+        <SFBadge {...args}>
+          <SFIconButton sfSize='medium' sfIcon='Bell' />
+        </SFBadge>
+      </div>
+      <h4>Position adjusted</h4>
+      <div style={{ display: 'flex', gap: '30px', alignItems: 'center' }}>
+        <SFBadge {...args}>
+          <SFIcon icon='Bell' />
+        </SFBadge>
+        <SFBadge className={style.custom} {...args}>
+          <SFIconButton sfSize='medium' sfIcon='Bell' />
+        </SFBadge>
+      </div>
+    </Fragment>
+  );
+};
+
+export const BadgeRectangular = (args): JSX.Element => {
+  const style = useRectangleStyles();
+
+  return (
+    <Fragment>
+      {headerNote()}
+      <h4>Default config</h4>
+      <div style={{ display: 'flex', gap: '30px', alignItems: 'center' }}>
+        <SFBadge {...args}>
+          <p style={{ margin: 0, padding: 0 }}>Text</p>
+        </SFBadge>
+        <SFBadge {...args}>
+          <SFLink sfSize='medium'>Some Link</SFLink>
+        </SFBadge>
+      </div>
+      <h4>Position adjusted</h4>
+      <div style={{ display: 'flex', gap: '30px', alignItems: 'center' }}>
+        <SFBadge className={style.custom} {...args}>
+          <p style={{ margin: 0, padding: 0 }}>Text</p>
+        </SFBadge>
+        <SFBadge {...args}>
+          <SFLink sfSize='medium'>Some Link</SFLink>
+        </SFBadge>
+      </div>
+    </Fragment>
+  );
+};
 
 BadgeCircular.args = {
   ...BadgeCircular.args,
