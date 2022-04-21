cd7a45ab6807d38fe09f631a2d302b4ae5a23e5b
Martin Weingart <martinweingart@gmail.com>
2022-04-21 10:40:30 -0300

    Update SFTooltip component

--- src/Components/SFTootltip/SFTooltip.tsx
+++ src/Components/SFTootltip/SFTooltip.tsx
@@ -1,77 +1,30 @@
 import React from 'react';
-import { makeStyles, Theme } from '@material-ui/core/styles';
-import { Tooltip, TooltipProps } from '@material-ui/core';
+import { styled, Tooltip, TooltipProps } from '@mui/material';
 import { SFGrey, SFRed } from '../../SFColors/SFColors';
 
-const getBackgroundColor = (
-  props: Partial<SFTooltipProps>,
-  isLight: boolean
-): string => {
-  if (props.sfColor === 'red') {
+const getBackgroundColor = (sfColor: string, isLight: boolean): string => {
+  if (sfColor === 'red') {
     return isLight ? SFRed[700] : SFRed[200];
   } else {
     return isLight ? SFGrey[800] : (SFGrey.A100 as string);
   }
 };
 
-const getTitleColor = (
-  props: Partial<SFTooltipProps>,
-  isLight: boolean
-): string => {
-  if (props.sfColor === 'red') {
+const getTitleColor = (sfColor: string, isLight: boolean): string => {
+  if (sfColor === 'red') {
     return isLight ? SFRed[50] : SFRed[900];
   } else {
     return isLight ? SFGrey[50] : SFGrey[900];
   }
 };
 
-const getContentColor = (
-  props: Partial<SFTooltipProps>,
-  isLight: boolean
-): string => {
-  if (props.sfColor === 'red') {
+const getContentColor = (sfColor: string, isLight: boolean): string => {
+  if (sfColor === 'red') {
     return isLight ? SFRed[100] : SFRed[800];
   } else {
     return isLight ? (SFGrey.A200 as string) : SFGrey[700];
   }
 };
-
-const useStyles = makeStyles((theme: Theme) => ({
-  tooltip: {
-    padding: '5px 12px',
-    backgroundColor: (props: Partial<SFTooltipProps>): string =>
-      getBackgroundColor(props, theme.palette.type === 'light')
-  },
-  arrow: {
-    color: (props: Partial<SFTooltipProps>): string =>
-      getBackgroundColor(props, theme.palette.type === 'light')
-  },
-  tooltipPlacementTop: {
-    margin: '0 0 5px 0'
-  },
-  tooltipPlacementLeft: {
-    margin: '0 5px 0 0'
-  },
-  tooltipPlacementRight: {
-    margin: '0 0 0 5px'
-  },
-  tooltipPlacementBottom: {
-    margin: '5px 0 0 0'
-  },
-  title: {
-    fontWeight: 500,
-    fontSize: '13px',
-    lineHeight: '22px',
-    color: (props: Partial<SFTooltipProps>): string =>
-      getTitleColor(props, theme.palette.type === 'light')
-  },
-  content: {
-    fontSize: '12px',
-    lineHeight: '18px',
-    color: (props: Partial<SFTooltipProps>): string =>
-      getContentColor(props, theme.palette.type === 'light')
-  }
-}));
 
 export interface SFTooltipProps extends TooltipProps {
   title: string;
@@ -79,25 +32,62 @@ export interface SFTooltipProps extends TooltipProps {
   sfColor?: 'default' | 'red';
 }
 
-export const SFTooltip = ({
+const SFTooltipBase = ({
+  className = '',
   title,
   content,
   sfColor = 'default',
   arrow = true,
   ...props
 }: SFTooltipProps): React.ReactElement<SFTooltipProps> => {
-  const { title: classTitle, content: classContent, ...classes } = useStyles({
-    sfColor
-  });
-
   const tooltipTitle: React.ReactNode = (
     <React.Fragment>
-      <span className={classTitle}>{title}</span>
-      {content && <div className={classContent}>{content}</div>}
+      <span className='SFTooltip-title'>{title}</span>
+      {content && <div className='SFTooltip-content'>{content}</div>}
     </React.Fragment>
   );
 
   return (
-    <Tooltip classes={classes} arrow={arrow} title={tooltipTitle} {...props} />
+    <Tooltip
+      arrow={arrow}
+      title={tooltipTitle}
+      {...props}
+      className={className}
+    />
   );
 };
+
+export const SFTooltip = styled(({ className, ...props }: SFTooltipProps) => (
+  <SFTooltipBase {...props} classes={{ popper: className }} />
+))(({ theme, sfColor = 'default' }) => ({
+  '& .MuiTooltip-tooltip': {
+    padding: '5px 12px',
+    backgroundColor: getBackgroundColor(sfColor, theme.palette.mode === 'light')
+  },
+  '&	.MuiTooltip-arrow': {
+    color: getBackgroundColor(sfColor, theme.palette.mode === 'light')
+  },
+  '&[data-popper-placement*="top"] .MuiTooltip-tooltipPlacementTop': {
+    marginBottom: '5px'
+  },
+  '&[data-popper-placement*="left"] .MuiTooltip-tooltipPlacementLeft': {
+    marginRight: '5px'
+  },
+  '&[data-popper-placement*="right"] .MuiTooltip-tooltipPlacementRight': {
+    marginLeft: '5px'
+  },
+  '&[data-popper-placement*="bottom"] .MuiTooltip-tooltipPlacementBottom': {
+    marginTop: 5
+  },
+  '& .SFTooltip-title': {
+    fontWeight: 500,
+    fontSize: '13px',
+    lineHeight: '22px',
+    color: getTitleColor(sfColor, theme.palette.mode === 'light')
+  },
+  '& .SFTooltip-content': {
+    fontSize: '12px',
+    lineHeight: '18px',
+    color: getContentColor(sfColor, theme.palette.mode === 'light')
+  }
+}));
