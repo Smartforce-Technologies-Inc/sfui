5568e6c877f12958111f5e534190fb8313daa709
Martin Weingart <martinweingart@gmail.com>
2022-04-22 16:24:01 -0300

    Update SFTimeline component

--- src/Components/SFTimeline/SFTimeline.tsx
+++ src/Components/SFTimeline/SFTimeline.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-
+import { styled } from '@mui/material/styles';
 import {
   Timeline,
   TimelineConnector,
@@ -7,71 +7,60 @@ import {
   TimelineDot,
   TimelineItem,
   TimelineSeparator
-} from '@material-ui/lab';
+} from '@mui/lab';
 
-import { makeStyles, Theme, withStyles } from '@material-ui/core';
 import { SFBlue, SFGrey } from '../../SFColors/SFColors';
 import { hexToRgba } from '../../Helpers';
 
-const StyledTimelineDot = withStyles((theme: Theme) => ({
-  root: {
-    backgroundColor: theme.palette.type === 'light' ? SFBlue[500] : SFBlue[200],
-    margin: '0px',
-    padding: '2px',
-    boxShadow: 'none'
-  }
-}))(TimelineDot);
+const StyledTimelineDot = styled(TimelineDot)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'light' ? SFBlue[500] : SFBlue[200],
+  margin: '0px',
+  padding: '2px',
+  boxShadow: 'none'
+}));
 
-const StyledTimelineContent = withStyles((theme: Theme) => ({
-  root: {
-    padding: '12px',
-    marginTop: '-16px',
-    marginBottom: '16px',
+const StyledTimelineContent = styled(TimelineContent)(({ theme }) => ({
+  padding: '12px',
+  marginTop: '-16px',
+  marginBottom: '16px',
 
-    '&:hover': {
-      '@media (hover: hover)': {
-        backgroundColor:
-          theme.palette.type === 'light'
-            ? hexToRgba(SFBlue[100], 0.4)
-            : hexToRgba(SFBlue[200], 0.2)
-      }
+  '&:hover': {
+    '@media (hover: hover)': {
+      backgroundColor:
+        theme.palette.mode === 'light'
+          ? hexToRgba(SFBlue[100], 0.4)
+          : hexToRgba(SFBlue[200], 0.2)
     }
   }
-}))(TimelineContent);
+}));
 
-const StyledTimelineItem = withStyles(() => ({
-  root: {
-    gap: '3px',
-    minHeight: 'auto',
+const StyledTimelineItem = styled(TimelineItem)({
+  gap: '3px',
+  minHeight: 'auto',
 
-    '&:hover': {
-      '@media (hover: hover)': {
-        cursor: 'pointer'
-      }
-    },
-
-    '&::before': {
-      content: 'none'
+  '&:hover': {
+    '@media (hover: hover)': {
+      cursor: 'pointer'
     }
-  }
-}))(TimelineItem);
+  },
 
-const StyledTimelineConnector = withStyles((theme: Theme) => ({
-  root: {
-    width: '0px',
-    backgroundColor: 'transparent',
-    borderLeft: `1px dashed ${
-      theme.palette.type === 'light' ? SFGrey[200] : SFGrey[500]
-    }`
+  '&::before': {
+    content: 'none'
   }
-}))(TimelineConnector);
+});
 
-const StyledTimeline = withStyles(() => ({
-  root: {
-    margin: '16px 0 0',
-    padding: 0
-  }
-}))(Timeline);
+const StyledTimelineConnector = styled(TimelineConnector)(({ theme }) => ({
+  width: '0px',
+  backgroundColor: 'transparent',
+  borderLeft: `1px dashed ${
+    theme.palette.mode === 'light' ? SFGrey[200] : SFGrey[500]
+  }`
+}));
+
+const StyledTimeline = styled(Timeline)({
+  margin: '16px 0 0',
+  padding: 0
+});
 
 export interface SFTimelineItem {
   title: string;
@@ -86,42 +75,16 @@ export interface SFTimelineProps {
   onItemClick?: (item: SFTimelineItem, index: number) => void;
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
-  text: {
-    fontSize: ({ size }: Partial<SFTimelineProps>): string =>
-      size === 'large' ? '16px' : '14px',
-    lineHeight: ({ size }: Partial<SFTimelineProps>): string =>
-      size === 'large' ? '24px' : '20px'
-  },
-  title: {
-    fontWeight: 500
-  },
-  subtitle: {
-    color: theme.palette.type === 'light' ? SFGrey[600] : SFGrey[400]
-  },
-  isSelected: {
-    backgroundColor:
-      theme.palette.type === 'light'
-        ? hexToRgba(SFBlue[100], 0.6)
-        : hexToRgba(SFBlue[200], 0.1)
-  },
-  lastItem: {
-    marginBottom: '0px'
-  }
-}));
-
-export const SFTimeline = ({
+const SFTimelineBase = ({
   className = '',
   items,
   selectedIndex = 0,
-  size = 'medium',
   onItemClick
 }: SFTimelineProps): React.ReactElement<SFTimelineProps> => {
-  const classes = useStyles({ size });
   const itemsLength: number = items.length;
 
   return (
-    <StyledTimeline align='left' className={className}>
+    <StyledTimeline position='right' className={className}>
       {items.map((item: SFTimelineItem, index: number) => {
         const hasConnector: boolean =
           (index > 0 && index < itemsLength - 1) ||
@@ -138,13 +101,13 @@ export const SFTimeline = ({
                 {hasConnector && <StyledTimelineConnector />}
               </TimelineSeparator>
               <StyledTimelineContent
-                className={`${classes.text} ${
-                  selectedIndex === index ? classes.isSelected : ''
-                } ${index === itemsLength - 1 ? classes.lastItem : ''}`}
+                className={`SFTimeline-text ${
+                  selectedIndex === index ? 'SFTimeline-isSelected' : ''
+                } ${index === itemsLength - 1 ? 'SFTimeline-lastItem' : ''}`}
               >
-                <span className={classes.title}>{item.title}</span>
+                <span className='SFTimeline-title'>{item.title}</span>
                 <br />
-                <span className={classes.subtitle}>{item.subtitle}</span>
+                <span className='SFTimeline-subtitle'>{item.subtitle}</span>
               </StyledTimelineContent>
             </StyledTimelineItem>
           </div>
@@ -153,3 +116,27 @@ export const SFTimeline = ({
     </StyledTimeline>
   );
 };
+
+export const SFTimeline = styled(SFTimelineBase)(
+  ({ theme, size = 'medium' }) => ({
+    '& .SFTimeline-text': {
+      fontSize: size === 'large' ? '16px' : '14px',
+      lineHeight: size === 'large' ? '24px' : '20px'
+    },
+    '& .SFTimeline-title': {
+      fontWeight: 500
+    },
+    '& .SFTimeline-subtitle': {
+      color: theme.palette.mode === 'light' ? SFGrey[600] : SFGrey[400]
+    },
+    '& .SFTimeline-isSelected': {
+      backgroundColor:
+        theme.palette.mode === 'light'
+          ? hexToRgba(SFBlue[100], 0.6)
+          : hexToRgba(SFBlue[200], 0.1)
+    },
+    '& .SFTimeline-lastItem': {
+      marginBottom: '0px'
+    }
+  })
+);
