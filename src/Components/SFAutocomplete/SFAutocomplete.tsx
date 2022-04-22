f65662c3cf8084c43c4d5f04a33cfd74159a38b6
Martin Weingart <martinweingart@gmail.com>
2022-04-22 17:02:10 -0300

    Update SFAutocomplete component

--- src/Components/SFAutocomplete/SFAutocomplete.tsx
+++ src/Components/SFAutocomplete/SFAutocomplete.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { Theme, withStyles, makeStyles } from '@material-ui/core/styles';
 import {
+  styled,
   Autocomplete,
   AutocompleteProps,
   AutocompleteInputChangeReason,
   AutocompleteChangeReason,
   AutocompleteCloseReason,
-  AutocompleteRenderInputParams
-} from '@material-ui/lab';
+  AutocompleteRenderInputParams,
+  Popper
+} from '@mui/material';
 import { SFMenuOption } from '../SFSelect/SFSelect';
 import { SFTextField } from '../SFTextField/SFTextField';
 import { SFIcon } from '../SFIcon/SFIcon';
@@ -22,8 +23,8 @@ const isOption = (value: string, options: SFMenuOption[]): boolean => {
   }
 };
 
-const StyledAutocomplete = withStyles((theme: Theme) => ({
-  inputRoot: {
+const StyledAutocomplete = styled(Autocomplete)(({ theme }) => ({
+  '& .MuiAutocomplete-inputRoot': {
     '&[class*="MuiOutlinedInput-root"]': {
       paddingTop: '20px',
 
@@ -36,66 +37,59 @@ const StyledAutocomplete = withStyles((theme: Theme) => ({
       }
     }
   },
-  endAdornment: {
+  '& .MuiAutocomplete-endAdornment': {
     marginTop: '-3px',
     '& button': {
       padding: '9px',
       '&:hover': {
         '@media (hover: hover)': {
           backgroundColor:
-            theme.palette.type === 'light'
+            theme.palette.mode === 'light'
               ? hexToRgba(SFGrey.A100 as string, 0.3)
               : hexToRgba(SFGrey[500] as string, 0.3)
         }
       },
       '&:active': {
         backgroundColor:
-          theme.palette.type === 'light'
+          theme.palette.mode === 'light'
             ? hexToRgba(SFGrey.A100 as string, 0.5)
             : hexToRgba(SFGrey[500] as string, 0.5)
       }
     }
-  },
-  listbox: {
+  }
+}));
+
+const StyledPopper = styled(Popper)(({ theme }) => ({
+  '& .MuiAutocomplete-listbox': {
     padding: '13px 0',
     backgroundColor:
-      theme.palette.type === 'light' ? SFSurfaceLight : SFGrey[800]
-  },
-  option: {
-    padding: '6px 24px',
+      theme.palette.mode === 'light' ? SFSurfaceLight : SFGrey[800],
 
-    '&[data-focus="true"]': {
-      backgroundColor:
-        theme.palette.type === 'light'
-          ? hexToRgba(SFGrey.A100 as string, 0.3)
-          : hexToRgba(SFGrey[500] as string, 0.3),
-      '&:active': {
+    '& .MuiAutocomplete-option': {
+      padding: '6px 24px',
+
+      '&.Mui-focused': {
         backgroundColor:
-          theme.palette.type === 'light'
+          theme.palette.mode === 'light'
+            ? hexToRgba(SFGrey.A100 as string, 0.3)
+            : hexToRgba(SFGrey[500] as string, 0.3),
+        '&:active': {
+          backgroundColor:
+            theme.palette.mode === 'light'
+              ? hexToRgba(SFGrey.A100 as string, 0.5)
+              : hexToRgba(SFGrey[500] as string, 0.5)
+        }
+      },
+
+      '&[aria-selected="true"]': {
+        backgroundColor:
+          theme.palette.mode === 'light'
             ? hexToRgba(SFGrey.A100 as string, 0.5)
             : hexToRgba(SFGrey[500] as string, 0.5)
       }
-    },
-
-    '&[aria-selected="true"]': {
-      backgroundColor:
-        theme.palette.type === 'light'
-          ? hexToRgba(SFGrey.A100 as string, 0.5)
-          : hexToRgba(SFGrey[500] as string, 0.5)
     }
   }
-}))(Autocomplete);
-
-const useStyles = makeStyles({
-  root: {
-    '& button.MuiAutocomplete-popupIndicator': {
-      display: (props: Partial<SFAutocompleteProps>): string =>
-        props.hasPopupIcon ? 'inline-flex' : 'none',
-      padding: (props: Partial<SFAutocompleteProps>): string =>
-        props.hasPopupIcon ? '9px' : '0'
-    }
-  }
-});
+}));
 
 export type SFAutocompleteInputChangeReason = AutocompleteInputChangeReason;
 export type SFAutocompleteChangeReason = AutocompleteChangeReason;
@@ -119,8 +113,7 @@ export interface SFAutocompleteProps
   value?: string;
   onChange: (value: string) => void;
 }
-
-export const SFAutocomplete = ({
+const SFAutocompleteBase = ({
   label,
   required = false,
   hasPopupIcon = false,
@@ -128,7 +121,7 @@ export const SFAutocomplete = ({
   value,
   ...props
 }: SFAutocompleteProps): React.ReactElement<SFAutocompleteProps> => {
-  const classes = useStyles({ hasPopupIcon });
+  // const classes = useStyles({ hasPopupIcon });
 
   let initInputValue = '';
   if (value && isOption(value, props.options)) {
@@ -158,7 +151,7 @@ export const SFAutocomplete = ({
     option: SFMenuOption,
     reason: AutocompleteChangeReason
   ): void => {
-    if (reason !== 'create-option' && reason !== 'remove-option') {
+    if (reason !== 'createOption' && reason !== 'removeOption') {
       setInputValue(option ? option.value : '');
       props.onChange(option ? option.value : '');
     }
@@ -172,7 +165,7 @@ export const SFAutocomplete = ({
 
   return (
     <StyledAutocomplete
-      className={`${classes.root} ${props.className || ''}`}
+      className={`SFAutocomplete-root ${props.className || ''}`}
       {...props}
       openOnFocus
       value={value}
@@ -180,7 +173,7 @@ export const SFAutocomplete = ({
       onChange={onChange}
       onInputChange={onInputChange}
       inputValue={inputValue}
-      getOptionSelected={(
+      isOptionEqualToValue={(
         option: SFMenuOption,
         value: SFMenuOption | string
       ): boolean => {
@@ -195,7 +188,17 @@ export const SFAutocomplete = ({
         <SFTextField {...params} label={label} required={required} />
       )}
       popupIcon={<SFIcon icon='Down-2' size={16} />}
-      closeIcon={<SFIcon icon='Close' size={16} />}
+      clearIcon={<SFIcon icon='Close' size={16} />}
+      PopperComponent={StyledPopper}
     />
   );
 };
+
+export const SFAutocomplete = styled(SFAutocompleteBase)(
+  ({ hasPopupIcon }) => ({
+    '& button.MuiAutocomplete-popupIndicator': {
+      display: hasPopupIcon ? 'inline-flex' : 'none',
+      padding: hasPopupIcon ? '9px' : '0'
+    }
+  })
+);
