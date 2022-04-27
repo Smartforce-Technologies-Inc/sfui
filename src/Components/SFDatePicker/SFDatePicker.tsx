f34c5783fd878cc55030cbbdaf350ff90fdb2cf8
Martin Weingart <martinweingart@gmail.com>
2022-04-27 09:10:10 -0300

    Starting to migrate SFDatePicker

--- src/Components/SFDatePicker/SFDatePicker.tsx
+++ src/Components/SFDatePicker/SFDatePicker.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
 import {
-  createStyles,
-  makeStyles,
-  Theme,
-  withStyles
-} from '@material-ui/core/styles';
-import MomentUtils from '@date-io/moment';
+  LocalizationProvider,
+  DatePicker,
+  DatePickerProps,
+  PickersDay,
+  PickersDayProps
+} from '@mui/x-date-pickers';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { SFTextField, SFTextFieldProps } from '../SFTextField/SFTextField';
 import {
   SFBlue,
   SFBlueMainDark,
@@ -17,344 +20,57 @@ import {
   SFTextWhite
 } from '../../SFColors/SFColors';
 import { SFIcon } from '../SFIcon/SFIcon';
-import {
-  KeyboardDatePicker,
-  MuiPickersUtilsProvider,
-  KeyboardDatePickerProps
-} from '@material-ui/pickers';
 import { hexToRgba } from '../../Helpers';
+import { SFIconButton, SFIconButtonProps } from '../SFIconButton/SFIconButton';
+import { TextFieldProps } from '@mui/material';
 
-const useButtonBackgrounds = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      backgroundColor: `${
-        theme.palette.type !== 'light' ? SFGrey[800] : undefined
-      }`
-    }
-  })
-);
+// const useButtonBackgrounds = makeStyles((theme: Theme) =>
+//   createStyles({
+//     root: {
+//       backgroundColor: `${
+//         theme.palette.mode !== 'light' ? SFGrey[800] : undefined
+//       }`
+//     }
+//   })
+// );
 
-const usePopOverStyle = makeStyles((theme: Theme) =>
-  createStyles({
-    paper: {
-      marginTop: '3px',
-      marginLeft: '-2px',
-      backgroundColor: `${
-        theme.palette.type !== 'light' ? SFGrey[800] : undefined
-      }`,
-      '& .MuiPickersBasePicker-container': {
-        paddingTop: 20
-      },
-      // Toolbar
-      '& .MuiPickersDatePickerRoot-toolbar': {
-        flexDirection: 'row-reverse',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        gap: 2,
-        height: 40,
-        minHeight: 40,
-        paddingLeft: 0,
-        paddingRight: 0,
-        backgroundColor: `${
-          theme.palette.type !== 'light' ? SFGrey[800] : SFSurfaceLight
-        }`,
-        width: '100%',
-        // Toolbar buttons
-        '& .MuiPickersToolbarButton-toolbarBtn': {
-          marginLeft: 24,
-          padding: '0 12px',
-          height: 34,
-          borderRadius: 34,
-          border: `1px solid ${
-            theme.palette.type !== 'light' ? SFBlueMainDark : SFBlueMainLight
-          }`,
-          '&:first-of-type': {
-            marginLeft: 0
-          },
-          '&:hover': {
-            backgroundColor: `${
-              theme.palette.type === 'light'
-                ? hexToRgba(SFBlue[100], 0.4)
-                : hexToRgba(SFBlue[200], 0.2)
-            }`
-          },
-          '&:active': {
-            backgroundColor: `${
-              theme.palette.type === 'light'
-                ? hexToRgba(SFBlue[100], 0.6)
-                : hexToRgba(SFBlue[200], 0.1)
-            }`
-          }
-        },
-        '& .MuiPickersToolbarText-toolbarBtnSelected, & .MuiPickersToolbarText-toolbarTxt': {
-          fontSize: 16,
-          lineHeight: '24px',
-          fontWeight: 400,
-          color: `${
-            theme.palette.type !== 'light' ? SFBlueMainDark : SFBlueMainLight
-          }`
-        }
-      },
-      // Calendar Header
-      '& .MuiPickersCalendarHeader-switchHeader': {
-        gap: 4,
-        padding: '0px 24px',
-        marginTop: 0,
-        marginBottom: 18,
-        '& .MuiPickersCalendarHeader-transitionContainer p': {
-          fontSize: 16,
-          lineHeight: '24px',
-          fontWeight: 400
-        }
-      },
-      // Day/Year Picker
-      '& .MuiPickersBasePicker-pickerView': {
-        maxWidth: 312,
-        minWidth: 312,
-        marginTop: 10,
-        '& .MuiPickersYearSelection-container': {
-          padding: '0 24px 0',
-          marginTop: 0,
-          '& .MuiPickersYear-root': {
-            display: 'inline-flex',
-            height: 36,
-            width: 62,
-            margin: 0,
-            fontSize: 14,
-            lineHeight: '20px',
-            borderRadius: 36,
-            '&:hover, &:active': {
-              color: `${
-                theme.palette.type !== 'light' ? SFTextWhite : SFTextBlack
-              }`,
-              backgroundColor: `${
-                theme.palette.type === 'light'
-                  ? hexToRgba(SFGrey[200], 0.3)
-                  : hexToRgba(SFGrey[500], 0.3)
-              }`
-            }
-          },
-          '& .MuiPickersYear-yearSelected': {
-            margin: 0,
-            color: `${
-              theme.palette.type !== 'light' ? SFTextBlack : SFTextWhite
-            }`,
-            backgroundColor: `${
-              theme.palette.type !== 'light' ? SFBlueMainDark : SFBlueMainLight
-            }`,
-            '&:hover': {
-              backgroundColor: `${
-                theme.palette.type === 'light' ? SFBlue[500] : SFBlue[200]
-              } !important`
-            }
-          }
-        }
-      },
-      '& .MuiPickersDay-current': {
-        border: `1px solid ${
-          theme.palette.type === 'light' ? SFBlue[500] : SFBlue[200]
-        }`
-      },
-      '& .MuiIconButton-root:hover': {
-        backgroundColor: `${
-          theme.palette.type === 'light'
-            ? hexToRgba(SFGrey[200], 0.3)
-            : hexToRgba(SFGrey[500], 0.3)
-        }`
-      },
-      '& .MuiPickersDay-daySelected:hover': {
-        backgroundColor: `${
-          theme.palette.type === 'light' ? SFBlue[500] : SFBlue[200]
-        }`
-      }
-    }
-  })
-);
+export interface SFDatePickerProps extends DatePickerProps {
+  className?: string;
+  error?: boolean;
+  helperText?: string;
+}
 
-const StyledDatePicker = withStyles((theme: Theme) => ({
-  root: {
-    boxSizing: 'border-box',
-
-    '&.openCalendarStyle': {
-      '& .MuiFilledInput-root': {
-        border: `2px solid ${
-          theme.palette.type === 'light' ? SFBlue[500] : SFBlue[200]
-        }`,
-
-        '& .MuiFilledInput-input': {
-          padding: '26px 10px 7px'
-        }
-      },
-      '& .MuiFilledInput-adornedEnd': {
-        paddingRight: '11px'
-      },
-      '& .MuiInputLabel-filled': {
-        '&.MuiInputLabel-shrink': {
-          color: theme.palette.type === 'light' ? SFBlue[500] : SFBlue[200]
-        }
-      }
-    },
-
-    '& .MuiFilledInput-root': {
-      height: '56px',
-      backgroundColor: theme.palette.background.paper,
-      border: `1px solid ${
-        theme.palette.type === 'light' ? SFGrey[200] : SFGrey[700]
-      }`,
-      borderRadius: 2,
-      boxSizing: 'border-box',
-
-      '&:before': {
-        content: `none !important`
-      },
-
-      '&:after': {
-        content: `none !important`
-      },
-
-      '&:hover': {
-        '@media (hover: hover)': {
-          borderColor: `${
-            theme.palette.type === 'light' ? SFGrey[900] : SFGrey[50]
-          }`
-        }
-      },
-
-      '&.Mui-focused': {
-        border: `2px solid ${
-          theme.palette.type === 'light' ? SFBlue[500] : SFBlue[200]
-        }`,
-
-        '& .MuiFilledInput-input': {
-          padding: '26px 10px 7px'
-        },
-        '& .MuiInputAdornment-root': {
-          marginRight: '-1px'
-        }
-      },
-
-      '&.Mui-error': {
-        border: `1px solid ${
-          theme.palette.type === 'light' ? SFRed[700] : SFRed[200]
-        } !important`,
-
-        '& .MuiFilledInput-input': {
-          padding: '26px 11px 7px !important'
-        },
-
-        '&.MuiFilledInput-adornedEnd': {
-          paddingRight: '12px !important'
-        }
-      },
-
-      '&.Mui-disabled': {
-        border: `1px solid ${
-          theme.palette.type === 'light' ? SFGrey[200] : SFGrey[700]
-        }`,
-
-        '& .MuiFilledInput-input': {
-          padding: '26px 11px 7px !important'
-        },
-
-        '&.MuiFilledInput-adornedEnd': {
-          paddingRight: '12px !important'
-        }
-      },
-
-      '& .MuiFilledInput-input': {
-        fontWeight: 400,
-        fontSize: '16px',
-        padding: '26px 11px 7px',
-
-        '&.Mui-disabled': {
-          color: `${theme.palette.type === 'light' ? SFGrey[200] : SFGrey[700]}`
-        },
-
-        '&.MuiFilledInput-adornedEnd': {
-          paddingRight: '12px'
-        }
-      },
-
-      '& .MuiIconButton-root': {
-        '@media (hover: hover)': {
-          '&:hover': {
-            backgroundColor: `${
-              theme.palette.type === 'light'
-                ? hexToRgba(SFGrey[200], 0.3)
-                : hexToRgba(SFGrey[500], 0.3)
-            }`
-          }
-        },
-
-        '&:active': {
-          backgroundColor: `${
-            theme.palette.type === 'light'
-              ? hexToRgba(SFGrey[200], 0.5)
-              : hexToRgba(SFGrey[500], 0.2)
-          }`
-        }
-      }
-    },
-
-    '& .MuiInputLabel-filled': {
-      fontSize: '16px',
-      lineHeight: '24px',
-      color: `${theme.palette.type === 'light' ? SFGrey[600] : SFGrey[400]}`,
-
-      '&.MuiInputLabel-shrink': {
-        fontSize: '14px',
-        lineHeight: '20px',
-        transform: `translate(12px, 6px)`,
-
-        '&.Mui-focused': {
-          color: theme.palette.type === 'light' ? SFBlue[500] : SFBlue[200]
-        }
-      },
-
-      '&.Mui-error': {
-        color: `${
-          theme.palette.type === 'light' ? SFRed[700] : SFRed[200]
-        } !important`
-      },
-
-      '&.Mui-disabled': {
-        color: `${theme.palette.type === 'light' ? SFGrey[200] : SFGrey[700]}`
-      }
-    },
-
-    '& .MuiFormHelperText-root': {
-      backgroundColor: 'transparent',
-
-      '&.Mui-error': {
-        color: `${theme.palette.type === 'light' ? SFRed[700] : SFRed[200]}`
-      }
+const CustomPickersDay = styled(PickersDay)(({ theme }) => ({
+  '&.MuiPickersDay-today': {
+    backgroundColor: 'transparent'
+  },
+  '&.Mui-selected': {
+    backgroundColor: 'red',
+    '&:hover, &:active': {
+      backgroundColor: 'red'
     }
   }
-}))(KeyboardDatePicker);
+})) as React.ComponentType<PickersDayProps<Date>>;
 
-export interface SFDatePickerProps extends KeyboardDatePickerProps {}
-
-export const SFDatePicker = ({
+const SFDatePickerBase = ({
   value = null,
   label,
+  error = false,
+  helperText = '',
   ...props
-}: SFDatePickerProps): React.ReactElement<KeyboardDatePickerProps> => {
-  const popOverStyle: Record<'paper', string> = usePopOverStyle();
-  const arrowStyle: Record<'root', string> = useButtonBackgrounds();
+}: SFDatePickerProps): React.ReactElement<SFDatePickerProps> => {
+  // const popOverStyle: Record<'paper', string> = usePopOverStyle();
+  // const arrowStyle: Record<'root', string> = useButtonBackgrounds();
   const [openCalendarStyle, setOpenCalendarStyle] = React.useState<boolean>(
     false
   );
 
   return (
-    <MuiPickersUtilsProvider utils={MomentUtils}>
-      <StyledDatePicker
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <DatePicker
         {...props}
-        fullWidth
-        className={openCalendarStyle ? 'openCalendarStyle' : ''}
+        inputFormat='MM/DD/YYYY'
         value={value}
-        variant='inline'
-        inputVariant='filled'
-        format='MM/DD/YYYY'
         label={label}
         onOpen={(): void => {
           setOpenCalendarStyle(true);
@@ -362,17 +78,172 @@ export const SFDatePicker = ({
         onClose={(): void => {
           setOpenCalendarStyle(false);
         }}
-        PopoverProps={{
-          classes: popOverStyle,
-          anchorOrigin: { vertical: 'bottom', horizontal: 'left' },
-          transformOrigin: { vertical: 'top', horizontal: 'left' }
-        }}
-        rightArrowButtonProps={{ classes: arrowStyle }}
-        rightArrowIcon={<SFIcon icon='Right-2' size='10' />}
-        leftArrowButtonProps={{ classes: arrowStyle }}
-        leftArrowIcon={<SFIcon icon='Left-2' size='10' />}
-        keyboardIcon={<SFIcon icon='Callendar' size='24' />}
+        // DialogProps={{
+        //   classes: popOverStyle,
+        //   anchorOrigin: { vertical: 'bottom', horizontal: 'left' },
+        //   transformOrigin: { vertical: 'top', horizontal: 'left' }
+        // }}
+        renderInput={(
+          props: TextFieldProps
+        ): React.ReactElement<SFTextFieldProps> => (
+          <SFTextField
+            {...props}
+            fullWidth
+            variant='outlined'
+            helperText={helperText}
+            error={error}
+            focused={openCalendarStyle || undefined}
+          />
+        )}
+        renderDay={(
+          day: Date,
+          selectedDates: Array<Date | null>,
+          pickersDayProps: PickersDayProps<Date>
+        ) => <CustomPickersDay {...pickersDayProps} />}
       />
-    </MuiPickersUtilsProvider>
+    </LocalizationProvider>
   );
 };
+
+export const SFDatePicker = styled(
+  ({ className, ...props }: SFDatePickerProps) => (
+    <SFDatePickerBase {...props} />
+  )
+)(({ theme }) => ({
+  marginTop: '3px',
+  marginLeft: '-2px',
+  backgroundColor: `${
+    theme.palette.mode !== 'light' ? SFGrey[800] : undefined
+  }`,
+
+  '& .MuiPickersBasePicker-container': {
+    paddingTop: 20
+  },
+
+  // Toolbar
+  '& .MuiPickersDatePickerRoot-toolbar': {
+    flexDirection: 'row-reverse',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    gap: 2,
+    height: 40,
+    minHeight: 40,
+    paddingLeft: 0,
+    paddingRight: 0,
+    backgroundColor: `${
+      theme.palette.mode !== 'light' ? SFGrey[800] : SFSurfaceLight
+    }`,
+    width: '100%',
+    // Toolbar buttons
+    '& .MuiPickersToolbarButton-toolbarBtn': {
+      marginLeft: 24,
+      padding: '0 12px',
+      height: 34,
+      borderRadius: 34,
+      border: `1px solid ${
+        theme.palette.mode !== 'light' ? SFBlueMainDark : SFBlueMainLight
+      }`,
+      '&:first-of-type': {
+        marginLeft: 0
+      },
+      '&:hover': {
+        backgroundColor: `${
+          theme.palette.mode === 'light'
+            ? hexToRgba(SFBlue[100], 0.4)
+            : hexToRgba(SFBlue[200], 0.2)
+        }`
+      },
+      '&:active': {
+        backgroundColor: `${
+          theme.palette.mode === 'light'
+            ? hexToRgba(SFBlue[100], 0.6)
+            : hexToRgba(SFBlue[200], 0.1)
+        }`
+      }
+    },
+    '& .MuiPickersToolbarText-toolbarBtnSelected, & .MuiPickersToolbarText-toolbarTxt': {
+      fontSize: 16,
+      lineHeight: '24px',
+      fontWeight: 400,
+      color: `${
+        theme.palette.mode !== 'light' ? SFBlueMainDark : SFBlueMainLight
+      }`
+    }
+  },
+
+  // Calendar Header
+  '& .MuiPickersCalendarHeader-switchHeader': {
+    gap: 4,
+    padding: '0px 24px',
+    marginTop: 0,
+    marginBottom: 18,
+    '& .MuiPickersCalendarHeader-transitionContainer p': {
+      fontSize: 16,
+      lineHeight: '24px',
+      fontWeight: 400
+    }
+  },
+
+  // Day/Year Picker
+  '& .MuiPickersBasePicker-pickerView': {
+    maxWidth: 312,
+    minWidth: 312,
+    marginTop: 10,
+    '& .MuiPickersYearSelection-container': {
+      padding: '0 24px 0',
+      marginTop: 0,
+      '& .MuiPickersYear-root': {
+        display: 'inline-flex',
+        height: 36,
+        width: 62,
+        margin: 0,
+        fontSize: 14,
+        lineHeight: '20px',
+        borderRadius: 36,
+        '&:hover, &:active': {
+          color: `${
+            theme.palette.mode !== 'light' ? SFTextWhite : SFTextBlack
+          }`,
+          backgroundColor: `${
+            theme.palette.mode === 'light'
+              ? hexToRgba(SFGrey[200], 0.3)
+              : hexToRgba(SFGrey[500], 0.3)
+          }`
+        }
+      },
+      '& .MuiPickersYear-yearSelected': {
+        margin: 0,
+        color: `${theme.palette.mode !== 'light' ? SFTextBlack : SFTextWhite}`,
+        backgroundColor: `${
+          theme.palette.mode !== 'light' ? SFBlueMainDark : SFBlueMainLight
+        }`,
+        '&:hover': {
+          backgroundColor: `${
+            theme.palette.mode === 'light' ? SFBlue[500] : SFBlue[200]
+          } !important`
+        }
+      }
+    }
+  }
+
+  // '& .MuiPickersDay-root': {
+  //   '&.MuiPickersDay-today': {
+  //     // color: theme.palette.mode === 'light' ? SFBlue[500] : SFBlue[200],
+  //     border: `1px solid ${
+  //       theme.palette.mode === 'light' ? SFBlue[500] : SFBlue[200]
+  //     }`
+  //   },
+
+  //   '&.Mui-selected': {
+  //     backgroundColor: `${
+  //       theme.palette.mode === 'light' ? SFBlue[500] : SFBlue[200]
+  //     }`,
+
+  //     '&:hover': {
+  //       backgroundColor: `${
+  //         theme.palette.mode === 'light' ? SFBlue[500] : SFBlue[200]
+  //       }`
+  //     }
+  //   }
+  // }
+}));
