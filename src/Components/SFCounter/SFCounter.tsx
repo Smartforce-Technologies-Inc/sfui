c4a4cfd02bd6f51a9c05cd1596984442e0bc35eb
Yopo Nicolas <nicolas.yopo@smartforcetech.com>
2023-02-02 15:23:35 -0300

    Updated SFCounter component, implemented SFCounterButton

--- src/Components/SFCounter/SFCounter.tsx
+++ src/Components/SFCounter/SFCounter.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { makeStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-import { SFIcon } from '../SFIcon/SFIcon';
-import { SFGrey } from '../../SFColors/SFColors';
-import { hexToRgba } from '../../Helpers';
 import { SFNumericField } from '../SFNumericField/SFNumericField';
+import { SFCounterButton } from './SFCounterButton/SFCounterButton';
 
-const counterStyles = makeStyles((theme: Theme) => ({
+const counterStyles = makeStyles({
   counter: {
     display: 'grid',
     gridTemplateColumns: '42px 108px 42px',
@@ -34,67 +32,8 @@ const counterStyles = makeStyles((theme: Theme) => ({
         }
       }
     }
-  },
-  button: {
-    border: `1px solid ${
-      theme.palette.type === 'light' ? SFGrey[200] : SFGrey[700]
-    }`,
-    cursor: 'pointer',
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-
-    '&:hover': {
-      backgroundColor:
-        theme.palette.type === 'light'
-          ? hexToRgba(SFGrey[200], 0.3)
-          : hexToRgba(SFGrey[500], 0.3)
-    },
-    '&:active': {
-      backgroundColor:
-        theme.palette.type === 'light'
-          ? hexToRgba(SFGrey[200], 0.3)
-          : hexToRgba(SFGrey[500], 0.3),
-
-      '& svg path': {
-        fill: `${
-          theme.palette.type === 'light' ? SFGrey[800] : SFGrey[200]
-        } !important`
-      }
-    },
-
-    '& svg path': {
-      fill: `${
-        theme.palette.type === 'light' ? SFGrey[600] : SFGrey[400]
-      } !important`
-    }
-  },
-  buttonDisabled: {
-    border: `1px solid ${
-      theme.palette.type === 'light' ? SFGrey[200] : SFGrey[700]
-    }`,
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    '& svg path': {
-      fill: `${
-        theme.palette.type === 'light' ? SFGrey[200] : SFGrey[700]
-      } !important`
-    }
-  },
-  right: {
-    borderLeft: 'none',
-    borderRadius: '0 2px 2px 0'
-  },
-  left: {
-    borderRight: 'none',
-    borderRadius: '2px 0 0 2px'
   }
-}));
+});
 
 export interface SFCounterProps {
   disabled: boolean;
@@ -110,42 +49,30 @@ export const SFCounter = ({
   const classes = counterStyles();
   const isRemoveDisabled: boolean = disabled || value === 0;
 
-  const onButtonClick = (isAdd: boolean): void => {
-    const newValue = isAdd ? value + 1 : value - 1;
-    if (newValue < 0) {
-      onChange(0);
-    } else {
-      onChange(newValue);
-    }
+  const onIncrement = (): void => {
+    onChange(value + 1);
+  };
+
+  const onDecrement = (): void => {
+    const newValue: number = value - 1;
+
+    onChange(newValue < 0 ? 0 : newValue);
   };
 
   return (
     <div className={classes.counter}>
-      <div
-        className={`${
-          isRemoveDisabled ? classes.buttonDisabled : classes.button
-        } ${classes.left}`}
-        onClick={
-          isRemoveDisabled ? undefined : (): void => onButtonClick(false)
-        }
-      >
-        <SFIcon icon='Remove' />
-      </div>
+      <SFCounterButton
+        icon='Remove'
+        disabled={isRemoveDisabled}
+        onClick={onDecrement}
+      />
       <SFNumericField
         className={classes.input}
         disabled={disabled}
         value={value}
-        allowNegative={false}
         onChange={(event): void => onChange(+event.target.value)}
       />
-      <div
-        className={`${disabled ? classes.buttonDisabled : classes.button} ${
-          classes.right
-        }`}
-        onClick={disabled ? undefined : (): void => onButtonClick(true)}
-      >
-        <SFIcon icon='Add' />
-      </div>
+      <SFCounterButton icon='Add' disabled={disabled} onClick={onIncrement} />
     </div>
   );
 };
