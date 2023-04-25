af6955233f5b846913837ffe87b571c4eb221972
Yopo Nicolas <nicolas.yopo@smartforcetech.com>
2023-04-25 08:50:34 -0300

    Updated SFChip exports

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@
 // export * from './Components/SFBadge/SFBadge';
 // export * from './Components/SFButton/SFButton';
 // export * from './Components/SFCard/SFCard';
-// export * from './Components/SFChip/SFChip';
+export * from './Components/SFChip/SFChip';
 // export * from './Components/SFChipListInput/SFChipListInput';
 // export * from './Components/SFChipListField/SFChipListField';
 // export * from './Components/SFCheckbox/SFCheckbox';
