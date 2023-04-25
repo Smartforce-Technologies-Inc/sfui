22525522c05261e0edea11ae752f53269b490b9f
Yopo Nicolas <nicolas.yopo@smartforcetech.com>
2023-04-25 10:05:03 -0300

    Updated SFMenu & SFMenuItem component & story

--- src/Components/SFMenuItem/SFMenuItem.tsx
+++ src/Components/SFMenuItem/SFMenuItem.tsx
@@ -1,39 +1,38 @@
-import { withStyles, Theme } from '@material-ui/core/styles';
-import { MenuItem } from '@material-ui/core';
+import { MenuItem, styled } from '@mui/material';
 import { hexToRgba } from '../../Helpers';
 import { SFGrey } from '../../SFColors/SFColors';
 
-export const SFMenuItem = withStyles((theme: Theme) => ({
+export const SFMenuItem = styled(MenuItem)(({ theme }) => ({
   root: {
     padding: '6px 24px',
     height: 36,
     '&:hover': {
       '@media (hover: hover)': {
         background:
-          theme.palette.type === 'light'
+          theme.palette.mode === 'light'
             ? hexToRgba(SFGrey[200], 0.3)
             : hexToRgba(SFGrey[500], 0.3)
       }
     },
     '&:active': {
       background:
-        theme.palette.type === 'light'
+        theme.palette.mode === 'light'
           ? hexToRgba(SFGrey[200], 0.5)
           : hexToRgba(SFGrey[500], 0.2)
     },
     '&.Mui-selected': {
       background:
-        theme.palette.type === 'light'
+        theme.palette.mode === 'light'
           ? hexToRgba(SFGrey[200], 0.5)
           : hexToRgba(SFGrey[500], 0.2),
       '&:hover': {
         '@media (hover: hover)': {
           background:
-            theme.palette.type === 'light'
+            theme.palette.mode === 'light'
               ? hexToRgba(SFGrey[200], 0.3)
               : hexToRgba(SFGrey[500], 0.3)
         }
       }
     }
   }
-}))(MenuItem);
+}));
