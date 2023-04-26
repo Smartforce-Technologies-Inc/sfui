d8b336bd877b9a42c5aabda99b843efc581ed578
Martin Weingart <martinweingart@gmail.com>
2023-04-26 08:01:57 -0300

    Fix dot styles

--- src/Components/SFBadge/SFBadge.tsx
+++ src/Components/SFBadge/SFBadge.tsx
@@ -34,7 +34,7 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
     boxShadow:
       '0px 2px 1px -1px rgba(0, 0, 0, 0.02), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 1px 3px rgba(0, 0, 0, 0.12)',
 
-    '.MuiBadge-dot': {
+    '.badgeDot': {
       fill: theme.palette.mode === 'light' ? SFCommonWhite : SFCommonBlack
     }
   }
