673538496a19c5805fe755f9458fb84646d23c5c
Martín Weingart <martinweingart@gmail.com>
2023-05-03 17:10:38 -0300

    Migrate SFNumericField component (#284)

--- .storybook/main.js
+++ .storybook/main.js
@@ -15,6 +15,7 @@ module.exports = {
     '../src/Components/SFIconButton/*.stories.@(js|jsx|ts|tsx)',
     '../src/Components/SFLink/*.stories.@(js|jsx|ts|tsx)',
     '../src/Components/SFMenu/*.stories.@(js|jsx|ts|tsx)',
+    '../src/Components/SFNumericField/*.stories.@(js|jsx|ts|tsx)',
     '../src/Components/SFPaper/*.stories.@(js|jsx|ts|tsx)',
     '../src/Components/SFRadioGroup/SFRadio/*.stories.@(js|jsx|ts|tsx)',
     '../src/Components/SFRadioGroup/*.stories.@(js|jsx|ts|tsx)',
