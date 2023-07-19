ceec44eb729a94b37fcc365be129c9fb33a2ab8b
Yopo Nicolas <nicolas.yopo@smartforcetech.com>
2023-07-19 15:16:48 -0300

    Added ScrollThumb component

---
+++ src/Components/SFScrollable/ScrollBar/ScrollThumb/ScrollThumb.tsx
@@ -0,0 +1,52 @@
+import React, { CSSProperties } from 'react';
+import { styled } from '@mui/material';
+import { ScrollBarOrientationType } from '../ScrollBar';
+import { hexToRgba } from '../../../../Helpers';
+import { SFGrey } from '../../../../SFColors/SFColors';
+
+const StyledScrollThumb = styled('div')(({ theme }) => ({
+  position: 'absolute',
+  borderRadius: '3px',
+  backgroundColor:
+    theme.palette.mode === 'light'
+      ? hexToRgba(SFGrey.A100 as string, 0.3)
+      : hexToRgba(SFGrey[500], 0.3)
+}));
+
+export interface ScrollThumbProps {
+  onMouseDown: (event: React.MouseEvent<HTMLDivElement>) => void;
+  orientation: ScrollBarOrientationType;
+  spaceLeft: number;
+  visibleSpace: number;
+}
+
+const getScrollThumbStyles = (
+  orientation: ScrollBarOrientationType,
+  spaceLeft: number,
+  visibleSpace: number
+): CSSProperties => {
+  if (orientation === 'horizontal') {
+    return { width: visibleSpace, left: spaceLeft, height: '6px' };
+  }
+
+  return {
+    height: visibleSpace,
+    top: spaceLeft,
+    marginLeft: '3px',
+    width: '6px'
+  };
+};
+
+export const ScrollThumb = ({
+  onMouseDown,
+  orientation,
+  spaceLeft,
+  visibleSpace
+}: ScrollThumbProps): React.ReactElement<ScrollThumbProps> => {
+  return (
+    <StyledScrollThumb
+      style={{ ...getScrollThumbStyles(orientation, spaceLeft, visibleSpace) }}
+      onMouseDown={onMouseDown}
+    />
+  );
+};
