2493108e17cd152f70a900a59419b738fd0c566c
Martin Weingart <martinweingart@gmail.com>
2023-09-08 08:34:44 -0300

    Export transition from mui

--- src/SFUtils/SFUtils.tsx
+++ src/SFUtils/SFUtils.tsx
@@ -1,7 +1,10 @@
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { StylesProvider } from '@material-ui/core/styles';
-import { ClickAwayListener } from '@material-ui/core';
-
-export { useMediaQuery as useSFMediaQuery };
-export { StylesProvider as SFStylesProvider };
-export { ClickAwayListener as SFClickAwayListener };
+export {
+  useMediaQuery as useSFMediaQuery,
+  StylesProvider as SFStylesProvider,
+  ClickAwayListener as SFClickAwayListener,
+  Fade as SFade,
+  Collapse as SFCollapse,
+  Grow as SFGrow,
+  Slide as SFSlide,
+  Zoom as SFZoom
+} from '@material-ui/core';
