c1100b90fedd81aee2a65ac226a2482b13ab3120
Yopo Nicolas <nicolas.yopo@smartforcetech.com>
2024-01-10 10:53:20 -0300

    Updated SFCounter component

--- src/Components/SFCounter/SFCounter.tsx
+++ src/Components/SFCounter/SFCounter.tsx
@@ -7,32 +7,32 @@ const StyledCounter = styled('div')({
   display: 'grid',
   gridTemplateColumns: '42px 108px 42px',
   gridTemplateRows: '42px',
-  width: 'fit-content',
+  width: 'fit-content'
+});
 
-  '& .input': {
-    '& .MuiInputBase-root': {
-      height: '42px',
+const StyledNumericField = styled(SFNumericField)({
+  '& .MuiInputBase-root': {
+    height: '42px',
 
-      '& .MuiInputBase-input': {
-        padding: '12px 12px 11px',
-        textAlign: 'center',
-        fontSize: '16px',
-        lineHeight: '24px',
-        MozAppearance: 'textfield',
+    '& .MuiInputBase-input': {
+      padding: '12px 12px 11px',
+      textAlign: 'center',
+      fontSize: '16px',
+      lineHeight: '24px',
+      MozAppearance: 'textfield',
 
-        '&::-webkit-outer-spin-button': {
-          WebkitAppearance: 'none',
-          margin: 0
-        },
-        '&::-webkit-inner-spin-button': {
-          WebkitAppearance: 'none',
-          margin: 0
-        }
+      '&::-webkit-outer-spin-button': {
+        WebkitAppearance: 'none',
+        margin: 0
       },
-
-      '& .MuiOutlinedInput-notchedOutline': {
-        borderRadius: 0
+      '&::-webkit-inner-spin-button': {
+        WebkitAppearance: 'none',
+        margin: 0
       }
+    },
+
+    '& .MuiOutlinedInput-notchedOutline': {
+      borderRadius: 0
     }
   }
 });
@@ -65,8 +65,7 @@ export const SFCounter = ({
         disabled={isRemoveDisabled}
         onClick={onDecrement}
       />
-      <SFNumericField
-        className='input'
+      <StyledNumericField
         disabled={disabled}
         value={value}
         onChange={(event): void => onChange(+event.target.value)}
